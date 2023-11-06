54d7384a4ae458edb29b238bff1c5ebb22c5d111
Wasif0787 <wasifhussain787@gmail.com>
2023-11-06 22:59:53 +0530

    Watched till 1hr 10 min

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Button, Container } from "@chakra-ui/react"
+import { Route, Routes } from "react-router-dom"
+import UserPage from "./pages/UserPage"
+import PostPage from "./pages/PostPage"
+import Header from "./components/HEader"
+import './index.css'
+function App() {
+
+  return (
+    <Container maxW="620px">
+      <Header/>
+      <Routes>
+          <Route path="/:username" element={<UserPage/>}/>
+          <Route path="/:username/post/:pid" element={<PostPage/>}/>
+      </Routes>
+    </Container>
+  )
+}
+
+export default App
