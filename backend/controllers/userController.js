80e27ca165c0ff01852fe214dbf1c1ceafd7271d
Wasif0787 <wasifhussain787@gmail.com>
2023-11-09 14:55:29 +0530

    Watched till 2hr 40min

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -46,7 +46,7 @@ const loginUser = async (req, res) => {
     try {
         const { username, password } = req.body
         const user = await User.findOne({ username })
-        const isPasswordCorrect = await bcrypt.compare(password, user.password)
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "")
 
         if (!user || !isPasswordCorrect) return res.status(400).json({ message: "Invalid username or password" })
 
@@ -60,8 +60,55 @@ const loginUser = async (req, res) => {
 
     } catch (err) {
         res.status(500).json({ message: err.message })
-        console.log(`Error in signupuser : ${err.message}`);
+        console.log(`Error in loginuser : ${err.message}`);
     }
 }
 
-export { signupUser, loginUser }
+const logoutUser = async (req, res) => {
+    try {
+        res.cookie("jwt", "", { maxAge: 1 })
+        res.status(200).json({ message: "User loggedout successfully" })
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+        console.log(`Error in logoutuser : ${err.message}`);
+    }
+}
+
+const followUnFollowUser = async (req, res) => {
+    try {
+        const { id } = req.params
+
+        const userToModify = await User.findById(id)
+        const currentUser = await User.findById(req.user._id)
+
+        if (id === req.user._id) return res.status(400).json({ message: "You cannt follow/unfollow yourself" })
+        if (!currentUser || !userToModify) return res, staus(400).json({ message: "User not found" })
+        const isFollowing = currentUser.following.includes(id)
+        if (isFollowing) {
+            //Unfollowing user
+            await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } })
+            await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } })
+            res.status(200).json({ message: "User unfollowed successfully" })
+        } else {
+            //Following
+            await User.findByIdAndUpdate(req.user._id, { $push: { following: id } })
+            await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } })
+            res.status(200).json({ message: "User followed successfully" })
+        }
+
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+        console.log(`Error in followUnFollowUser : ${err.message}`);
+    }
+}
+
+const updateProfile= async (req,res)=>{
+    try {
+        
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+        console.log(`Error in updateProfile : ${err.message}`);
+    }
+}
+
+export { signupUser, loginUser, logoutUser, followUnFollowUser,updateProfile}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { loginUser, signupUser } from "../controllers/userController.js";
+import { followUnFollowUser, loginUser, logoutUser, signupUser, updateProfile } from "../controllers/userController.js";
+import protectRoute from "../middlewares/protectRoute.js";
 const router = express.Router();
 
 
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
+router.post("/logout", logoutUser);
+router.post("/follow/:id",protectRoute,followUnFollowUser);
+router.post("/upadte/:id",protectRoute,updateProfile);
 
 export default router;
