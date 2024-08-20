3605f9137520049c74e5040be578375e944480b2
Wasif0787 <wasifhussain787@gmail.com>
2024-08-20 21:35:54 +0530

    change position of backend api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-const backendUrl = process.env.VITE_API_BACKEND || 'http://localhost:5000';
 // https://vitejs.dev/config/
 export default defineConfig({
-
 	plugins: [react()],
 	server: {
+		port: 4000,
 		// Get rid of the CORS error
 		proxy: {
-			"/api": backendUrl,
+			"/api": 'https://threads-backend-kzv5.onrender.com',
 		},
 	},
 });
+// https://threads-backend-kzv5.onrender.com
